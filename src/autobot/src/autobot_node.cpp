6c000269c0b65b66800efe07e7c54ac85b1f8c15
GUru <gurouv@gmail.com>
2014-07-29 17:42:34 +0200

    Final

--- src/autobot/src/autobot_node.cpp
+++ src/autobot/src/autobot_node.cpp
@@ -54,59 +54,117 @@ using namespace std;
 #include <move_base_msgs/MoveBaseAction.h>
 #include <actionlib/client/simple_action_client.h>
 #include <tf/transform_broadcaster.h>
+#include <boost/thread.hpp>
+
+// Global variables
+float xCoordinate = 0.0;        // Variable to hold the Goal's x position
+bool goalEnded = false;
+bool goalThreadStarted = false;
 
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
-int main(int argc, char** argv)
+
+void spinThread()
 {
-    float xCoordinate = 0.0;
-
-    if(argc != 2)
-    {
-      cout << "Incorrect arguments" << endl;
-      return 0;
-    }
-    else
-    {
-      try {
-        xCoordinate = boost::lexical_cast<float>( argv[1] );
-      }
-      catch( boost::bad_lexical_cast const& ) {
-        cout << "Error: input string was not valid" << std::endl;
-        cout << "Cannot send goal to YouBot";
-        return 0;
-      }
-    }
-
-    ros::init(argc, argv, "navigation_goals");
-    MoveBaseClient ac("move_base", true);
-
-    while(!ac.waitForServer(ros::Duration(5.0)))
-    {
-            ROS_INFO("Waiting for the move_base action server");
-    }
-
-    move_base_msgs::MoveBaseGoal goal;
-    goal.target_pose.header.frame_id = "map";
-    goal.target_pose.header.stamp = ros::Time::now();
-    goal.target_pose.pose.position.x = xCoordinate;
-    goal.target_pose.pose.position.y = 0;
-    goal.target_pose.pose.orientation.w = 1.0;
-
-    ROS_INFO("Sending goal");
-    ac.sendGoal(goal);
-
-    ac.waitForResult();
-
-    if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-            ROS_INFO("You have arrived to the goal position");
-    else
-    {
-            ROS_INFO("The base failed for some reason");
-    }
-
-    return 0;
+  ros::spin();
 }
 
+void ConstructAndSendGoal(MoveBaseClient *ac)
+{
+  while (!ac->waitForServer(ros::Duration(5.0)))
+  {
+    ROS_INFO("Waiting for the move_base action server");
+  }
 
+  goalThreadStarted = true;
 
+  // Construct goal
+  move_base_msgs::MoveBaseGoal goal;
+  goal.target_pose.header.frame_id = "map";
+  goal.target_pose.header.stamp = ros::Time::now();
+  goal.target_pose.pose.position.x = xCoordinate;
+  goal.target_pose.pose.position.y = 0;
+  goal.target_pose.pose.orientation.w = 1.0;
+
+  // Send the goal and wait for the goal to complete or abort
+  ROS_INFO("Sending goal");
+  ac->sendGoal(goal);
+  ac->waitForResult();
+
+  // Check for the status of the goal
+  if (ac->getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+    ROS_INFO("You have arrived to the goal position");
+  else
+  {
+    ROS_INFO("The base failed for some reason");
+  }
+
+  // Set the goalEnded flag to true. This flag is set irrespective of whether
+  // the goal succeeded or failed.
+  goalEnded = true;
+
+  cout << "Press any key to exit";
+}
+
+int main(int argc, char** argv)
+{
+  if (argc != 2)
+  {
+    cout << "Incorrect arguments" << endl;
+    return 0;
+  }
+  else
+  {
+    try
+    {
+      xCoordinate = boost::lexical_cast<float>(argv[1]);
+    }
+    catch (boost::bad_lexical_cast const&)
+    {
+      cout << "Error: input string was not valid" << std::endl;
+      cout << "Cannot send goal to YouBot";
+      return 0;
+    }
+  }
+
+  ros::init(argc, argv, "navigation_goals");
+
+  // Spin the ROS thread
+  boost::thread spin_thread(&spinThread);
+
+  // Construct the MoveBaseClient Object
+  MoveBaseClient ac("move_base", true);
+
+  // Spin a thread to construct the goal and send it to move_base
+  boost::thread sendGoal_thread(&ConstructAndSendGoal, &ac);
+
+  // Monitor for key input to cancel the goal
+  while(goalEnded != true)
+  {
+    int ch;
+    ch = getchar();
+    if(goalEnded == true)
+    {
+      cout << "Goal Ended. Exiting program." << endl;
+    }
+    else
+    {
+      if ((ch == 'c') || (ch == 'C'))
+      {
+        ac.cancelAllGoals();
+      }
+      else
+      {
+        cout << "YouBot is still pursuing the goal. Press 'c' or 'C' to "
+                "cancel it." << endl;
+      }
+    }
+  }
+
+  // shutdown the node and join the thread back before exiting
+  ros::shutdown();
+  spin_thread.join();
+
+  // Exit
+  return 0;
+}
